2e2f8403355af2105149c5bf6b8274c8c9f2dc92
mitzaM <mitaman89@yahoo.com>
2025-06-29 00:41:37 +0300

    Add clock

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from constants import SCREEN_WIDTH
 
 def main():
     pygame.init()
+
+    dt = 0
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -15,6 +18,7 @@ def main():
 
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
